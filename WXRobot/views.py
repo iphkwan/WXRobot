ebb9fb4a0144d1386798cecc761220ef1ee46217
iphkwan <yexjun@gmail.com>
2013-03-25 20:13:24 +0800

    stupid bugs

--- WXRobot/views.py
+++ WXRobot/views.py
@@ -56,15 +56,7 @@ def paraseMsgXml(rootElem):
     return msg
 
 def getReplyXml(msg, replyContent):
-    reply ="""<xml>
-        <ToUserName><![CDATA[%s]]></ToUserName>
-        <FromUserName><![CDATA[%s]]></FromUserName>
-        <CreateTime>%s</CreateTime>
-        <MsgType><![CDATA[%s]]></MsgType>
-        <Content><![CDATA[%s]]></Content>
-        <FuncFlag>0</FuncFlag>
-        </xml>"""
+    extTpl = "<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[%s]]></MsgType><Content><![CDATA[%s]]></Content><FuncFlag>0</FuncFlag></xml>";
+    extTpl = extTpl % (msg['FromUserName'],msg['ToUserName'],str(int(time.time())),'text',replyContent)
+    return extTpl
 
-    reply = reply % (msg['FromUserName'],msg['ToUserName'],str(int(time.time())),'text',replyContent)
-
-    return reply
