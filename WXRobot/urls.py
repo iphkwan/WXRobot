c4eed424ac082fee2ef96385896ce6398f59296f
iphkwan <yexjun@gmail.com>
2013-03-25 13:15:13 +0800

    test

--- WXRobot/urls.py
+++ WXRobot/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'WXRobot.views.home', name='home'),
+    url(r'^$', 'WXRobot.views.weixin'),
     # url(r'^WXRobot/', include('WXRobot.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
